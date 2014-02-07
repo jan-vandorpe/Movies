7b8fcc7493f9c1b50759f8f8b717d049cf5eefe5
Jan Vandorpe <jan.vandorpe@vdab.be>
2014-02-07 11:54:56 +0100

    film toevoegen en verwijderen

--- site/js/app/movies.js
+++ site/js/app/movies.js
@@ -33,12 +33,22 @@ define([
     //VIEW: één film
     var FilmView = Backbone.View.extend({
         tagName: "div",
-        className: "film",
+        className: "film clearFix",
         template: $("#filmTemplate").html(),
         render: function() {
             var tmpl = _.template(this.template);
             this.$el.html(tmpl(this.model.toJSON()));
             return this;
+        },
+        events: {
+            'click .delete': 'deleteFilm'
+        },
+        deleteFilm: function(e) {
+            console.log('deleteFilm');
+            //delete model
+            this.model.destroy();
+            //delete view
+            this.remove()
         }
     })
     //VIEW: filmovericht
@@ -48,8 +58,18 @@ define([
         initialize: function (films) {
                     this.collection = new FilmCollectie(films); // data: object of JSON
                     this.$lijst     = this.get$Lijst() //maak ref naar lijst
+                    //Event Listeners
+                    //this.listenTo(this.collection, 'all', function(){this.debug()})
+                    this.listenTo(this.collection, 'add', this.renderFilm); //event listener voor add event Collection
+           
                     this.render();
-              },
+        },
+        events: {
+            "click #voegtoe": "addFilm"
+        },
+        debug: function(e, tekst){
+            console.log(e, tekst);
+        },
         get$Lijst:function(){
             return this.$el.find('.lijst')
         },
@@ -59,13 +79,33 @@ define([
             }, this);
             console.log("rendering collection: ", this.collection);
         },
-
         renderFilm: function (item) {
             var filmView = new FilmView({
                 model: item
             });
             this.$lijst.append(filmView.render().el);
-        }
+        },
+        addFilm: function(e){
+            e.preventDefault();
+            console.log("addfilm")
+            var formData    = {};
+            $velden         = $('#frmFilm').find('input, textarea'); //gebruik JQ find() want children() enkel één niveau
+            $velden.each(function(i, el){
+                if($(el).val()!= ''){
+                    formData[el.name] = $(el).val();
+                }              
+            })
+            //corrigeer filename die volledig pad bevat, we moeten enkel de filename hebben
+            if(formData.foto){
+                fotoFullPath    = formData.foto;
+                formData.foto   = /([^\\]+)$/.exec(fotoFullPath)[1];
+            }
+            //formData.release =  new Date(formData.release).getTime(); //niet nodig in Chrome dankzij kalenderwidget, wel in andere browsers?
+            console.log('addFilm formData: ', formData);
+            this.collection.add(new Film(formData)); //eerste versie zonder API
+            
+     }
+
     });
 
 
